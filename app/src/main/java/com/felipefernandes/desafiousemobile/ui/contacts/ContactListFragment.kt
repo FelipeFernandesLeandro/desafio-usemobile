cf793fd0b755e26991931f6de0c4a29b9406806c
Felipe Leandro <felipefernandes.sjc@hotmail.com>
2020-10-31 17:47:57 -0300

    Add text to speak search on contact list

--- app/src/main/java/com/felipefernandes/desafiousemobile/ui/contacts/ContactListFragment.kt
+++ app/src/main/java/com/felipefernandes/desafiousemobile/ui/contacts/ContactListFragment.kt
@@ -1,12 +1,18 @@
 package com.felipefernandes.desafiousemobile.ui.contacts
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +25,7 @@ import com.felipefernandes.desafiousemobile.extensions.navigateWithAnimations
 import com.felipefernandes.desafiousemobile.extensions.setViewVisibility
 import kotlinx.android.synthetic.main.fragment_contact_list.*
 import org.koin.android.viewmodel.ext.android.viewModel
+import java.util.Locale
 
 class ContactListFragment : Fragment() {
     private val viewModel: ContactsViewModel by viewModel()
@@ -36,6 +43,78 @@ class ContactListFragment : Fragment() {
 
         localContactList?.let {
             setContactsAdapter(it)
+        }
+
+        setUi()
+    }
+
+    private fun setContactsAdapter(contacts: List<ContactModel>) {
+        val contactsAdapter = ContactsAdapter(
+            contacts,
+            onClick = { id, name ->
+                navigateToProfile(id, name)
+            },
+            onNothingFound = { noResultsFound ->
+                setNotFoundTextVisibility(noResultsFound)
+            },
+        )
+
+        contactListFragmentRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            addItemDecoration(DividerItemDecoration(context))
+            adapter = contactsAdapter
+        }
+
+        contactListFragmentSearchInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                contactsAdapter.filter.filter(charSequence.toString())
+            }
+
+            override fun afterTextChanged(editable: Editable) {}
+        })
+    }
+
+    private fun setUi() {
+        contactListFragmentSearchInput.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (drawableTouchCondition(event)) {
+                    v.performClick()
+                    startVoiceToText()
+                }
+            }
+            false
+        }
+    }
+
+    private fun drawableTouchCondition(
+        event: MotionEvent,
+        drawableRightConstant: Int = 2
+    ): Boolean {
+        val editTextBounds =
+            contactListFragmentSearchInput.right - contactListFragmentSearchInput.compoundDrawables[drawableRightConstant].bounds.width()
+        return event.rawX >= editTextBounds
+    }
+
+    private fun startVoiceToText() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            //alterar p search
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+        }.also {
+            val local = Locale("pt", "BR")
+            it.putExtra(RecognizerIntent.EXTRA_LANGUAGE, local)
+        }.also {
+            it.putExtra(RecognizerIntent.EXTRA_PROMPT, R.string.profile_im_listening)
+        }
+        try {
+            startActivityForResult(intent, REQUEST_CODE_SPEAK_TO_TEXT)
+        } catch (e: ActivityNotFoundException) {
+            e.printStackTrace()
+            Toast.makeText(context, "Your device does not support STT.", Toast.LENGTH_LONG).show()
         }
     }
 
@@ -68,34 +147,6 @@ class ContactListFragment : Fragment() {
         loadContacts()
     }
 
-    private fun setContactsAdapter(contacts: List<ContactModel>) {
-        val contactsAdapter = ContactsAdapter(
-            contacts,
-            onClick = { id, name ->
-                navigateToProfile(id, name)
-            },
-            onNothingFound = { noResultsFound ->
-                setNotFoundTextVisibility(noResultsFound)
-            },
-        )
-
-        contactListFragmentRecyclerView.apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-            addItemDecoration(DividerItemDecoration(context))
-            adapter = contactsAdapter
-        }
-
-        contactListFragmentAutoCompleteText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
-            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-                contactsAdapter.filter.filter(charSequence.toString())
-            }
-
-            override fun afterTextChanged(editable: Editable) {}
-        })
-    }
-
     private fun loadContacts() {
         viewModel.fetchContactList()
     }
@@ -111,6 +162,21 @@ class ContactListFragment : Fragment() {
         contactListFragmentNotFoundText.setViewVisibility(visible)
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            REQUEST_CODE_SPEAK_TO_TEXT -> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+                    if (result.isNullOrEmpty().not()) {
+                        val recognizedText = result?.get(0)
+                        contactListFragmentSearchInput.setText(recognizedText)
+                    }
+                }
+            }
+        }
+    }
+
     override fun onDestroy() {
         viewModel.cancelPendingJobs()
         super.onDestroy()
@@ -124,5 +190,9 @@ class ContactListFragment : Fragment() {
     override fun onPause() {
         contactListFragmentPlaceholder.stopShimmer()
         super.onPause()
+    }
+
+    companion object {
+        private const val REQUEST_CODE_SPEAK_TO_TEXT = 1
     }
 }
